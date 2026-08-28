935cbb107e6101af86a1288fc1c774f59d46c59e
MakkyMgbemena <annastecias@gmail.com>
2026-08-28 00:50:22 -0400

    Deploy 2026-08-28 00:50

---
+++ vite.config.mjs
@@ -0,0 +1,24 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        automationPage: resolve(__dirname, 'business-processes-and-automation.html'),
+        dashboard: resolve(__dirname, 'client-dashboard.html'),
+        specialist: resolve(__dirname, 'specialist-dashboard.html'),
+        adCancelled: resolve(__dirname, 'ad-cancelled.html'),
+        adSuccess: resolve(__dirname, 'ad-success.html'),
+        bookingCancelled: resolve(__dirname, 'booking-cancelled.html'),
+        bookingSuccess: resolve(__dirname, 'booking-success.html'),
+        notFound: resolve(__dirname, '404.html'),
+      },
+    },
+  },
+});
